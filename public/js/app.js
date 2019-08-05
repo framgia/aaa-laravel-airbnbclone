8cc2a418bf580cc0f95b8900e74c4d4ae66e5f09
AubreyV <breyvicente@gmail.com>
2019-08-05 18:33:36 +0800

    Room publishing

--- public/js/app.js
+++ public/js/app.js
@@ -49507,8 +49507,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/inahsanjuan/Projects/tech-school/aaa-laravel-airbnbclone/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/inahsanjuan/Projects/tech-school/aaa-laravel-airbnbclone/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/aubrey/Projects/AirBnbCloneLaravel/aaa-laravel-airbnbclone/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/aubrey/Projects/AirBnbCloneLaravel/aaa-laravel-airbnbclone/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
